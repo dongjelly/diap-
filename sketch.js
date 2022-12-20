be10dc8fd2d254f7fc6c2f87065b9e71131c9a07
dongjelly <jeena1007@naver.com>
2022-12-20 20:16:50 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,39 +1,39 @@
 let img;
 let movers = [];
-let canvas2;
+let canvas;
 
 function setup() {
   createCanvas(400, 400);
     noStroke();
-  fill(250);
-  ellipse(200,200,10);
-  canvas2 = createGraphics(400, 400);
+  //fill(250);
+  //ellipse(200,300,100);
+ canvas = createGraphics(400, 400);
   img = loadImage('saram.png');
   
   
   xoff = 0;
   yoff = 0;
 
-  for (let i = 0; i < 80; i++) {
-    let m = new Mover(random(width), random(height), random(1, 4));
+  for (let i = 0; i < 100; i++) {
+    let m = new Mover(random(width), random(height), random(1, 0));
     movers.push(m);
   }
 }
 
 function draw() {
-  background(50);
-  image(canvas2, 0, 0);
+  background(320);
+  image(canvas, 0, 0);
   let mousePos = createVector(mouseX, mouseY);
 
   for (let i = 0; i < movers.length; i++) {
     let m = movers[i];
     m.update();
     m.show();
-    m.show2(canvas2);
+    m.show2(canvas);
 
     if (mouseIsPressed) {
       let f = p5.Vector.sub(mousePos, m.pos);
-      f.mult(0.01);
+      f.mult(0.0001);
       m.applyForce(f);
     }
   }
@@ -60,15 +60,14 @@ class Mover {
   }
   
   show() {
-    fill(0);
+    fill(100);
     image(img, this.pos.x, this.pos.y, img.width / 20, img.height / 20);
-    //circle(this.pos.x, this.pos.y, this.m);  
-     
+    //circle(this.pos.x, this.pos.y, this.m);   
   }
   
   show2(aCanvas) {
-    aCanvas.noStroke();
-    aCanvas.fill(random(100,150), random(100,150), random(200,250), 5);
-    aCanvas.circle(this.pos.x, this.pos.y, this.vel.mag()*2);
+   //aCanvas.noStroke();
+    aCanvas.fill(random(120, 250), random(120, 250), random(120, 250), 100);
+    aCanvas.circle(this.pos.x, this.pos.y, this.vel.mag()*1);
   }
 }
